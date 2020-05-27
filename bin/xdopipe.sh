d9386fd481fdf1a31f01facd4e3a9ae3c020bb2f
Architector #4 <23612841+Architector4@users.noreply.github.com>
2020-05-27 06:01:21 +0300

    Update xdopipe.sh
    blergh

--- bin/xdopipe.sh
+++ bin/xdopipe.sh
@@ -2,8 +2,9 @@
 # Receive text on stdin and type it out with xdotool
 # Optional arguments are prepended to each line
 while read line; do
-	sleep 0.1
-	xdotool type "$@" "${line}"
+	#sleep 0.1
+	xdotool type $@ "${line}"
 	# Keypad ENTER key sometimes work better than normal Return
-	xdotool key KP_Enter
+	#xdotool key KP_Enter
+	xdotool key $@ Return
 done

247df6094c5c5c4a865c05b315231c31a758e9ac
Architector #4 <architector4@mail.tilde.team>
2020-07-08 09:52:07 +0300

    redshift.sh - add wayland support

--- bin/redshift.sh
+++ bin/redshift.sh
@@ -11,7 +11,14 @@ if [ -f "$STATUSFILE" ] && [ "$1" != "on" ]; then
 	# Remove the status file!
 	rm $STATUSFILE
 	# Turn it off
-	redshift -x
+	if [ -n "$WAYLAND_DISPLAY" ]; then	# Running on Wayland
+		# When using AUR package "redshift-wayland-git", any
+		# invocation of redshift keeps being open until killed, and
+		# killing it resets the red tint, essentially doing what is needed.
+		killall redshift
+	else					# Running on X
+		redshift -x
+	fi
 else
 	# If it's not running or "on" parameter was sent,
 	# turn on redshift and create statusfile.

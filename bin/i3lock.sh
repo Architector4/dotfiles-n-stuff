b13349aded2b00bf6204ad014d92e27727c4a2b0
Architector #4 <architector4@mail.tilde.team>
2021-02-03 06:06:55 +0300

    i3lock.sh - add swaylock support

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Pick a random picture from ~/Pictures/Glitchy and lock with it
-i3lock -nti "$(find ~/Pictures/Glitchy | shuf -n 1)"
+i3lock -nti "$(find ~/Pictures/Glitchy | shuf -n 1)" ||
+swaylock -ti "$(find ~/Pictures/Glitchy | shuf -n 1)"

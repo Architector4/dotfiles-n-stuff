9ab223b22f14b29dfdb32731124f8db2a25bd92a
Architector #4 <architector4@mail.tilde.team>
2024-04-06 21:50:15 +0300

    clicker.sh - make clicking faster

--- bin/clicker.sh
+++ bin/clicker.sh
@@ -27,10 +27,10 @@ else
 	touch "$STATUSFILE"
 	while true;do
 		# Make this number higher for slower clicks, lower for faster.
-		sleep 0.03
+		sleep 0.008
 		xdotool mousedown $BUTTON
 		# Give it some time of being held, just in case
-		sleep 0.03
+		sleep 0.008
 		# Raise the button
 		xdotool mouseup $BUTTON
 	done
